2f0b2de2e67774eda48671c960f70313ab122764
Joseph Mawa <186199059+josephmawa@users.noreply.github.com>
2025-04-13 14:28:44 +0300

    chore: add text input field

--- src/window.js
+++ src/window.js
@@ -2,14 +2,17 @@ import GObject from "gi://GObject";
 import Gtk from "gi://Gtk";
 import Adw from "gi://Adw";
 import Gio from "gi://Gio";
+import GLib from "gi://GLib";
 
 import { Hexagon, Container } from "./hexagon.js";
+
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 export const SpellingbeeWindow = GObject.registerClass(
   {
     GTypeName: "SpellingbeeWindow",
     Template: getResourceURI("window.ui"),
-    InternalChildren: ["container"],
+    InternalChildren: ["container", "entry"],
   },
   class SpellingbeeWindow extends Adw.ApplicationWindow {
     constructor(application) {
@@ -19,6 +22,19 @@ export const SpellingbeeWindow = GObject.registerClass(
       this.loadStyles();
       this.bindSettings();
       this.setPreferredColorScheme();
+
+      /**
+       * NOTE
+       * You can't connect insert-text event to Gtk.Entry directly.
+       * Read more about it in the following reference docs:
+       * • https://gitlab.gnome.org/GNOME/gtk/-/issues/4315
+       * • https://docs.gtk.org/gtk4/iface.Editable.html#implementing-gtkeditable
+       */
+      this.signalId = this._entry
+        .get_delegate()
+        .connect("insert-text", this.insertTextHandler);
+
+      this._entry.connect("icon-press", this.entryIconPressHandler);
     }
 
     createUI = () => {
@@ -43,9 +59,57 @@ export const SpellingbeeWindow = GObject.registerClass(
       this._container.append(container);
     };
 
-    hexClickHandler(_hexagon, label) {
-      console.log(label);
+    entryIconPressHandler = (entry, iconPos) => {
+      if (Gtk.EntryIconPosition.SECONDARY === iconPos) {
+        entry.set_text("");
+      }
+    };
+
+    hexClickHandler = (_hexagon, label) => {
+      const labelUpperCase = label.toLocaleUpperCase("en-US");
+      this._entry.insert_text(
+        labelUpperCase,
+        labelUpperCase.length,
+        this._entry.text.length
+      );
+    };
+
+    submitWord(entry) {
+      console.log(entry.get_text());
     }
+
+    wordChanged(entry) {
+      // Move cursor to the end of the text
+      entry.set_position(-1);
+    }
+
+    insertTextHandler = (editable, text, length, position) => {
+      const textUpperCase = text.toLocaleUpperCase("en-US");
+
+      const signalId = GObject.signal_lookup("insert-text", editable);
+      const handlerId = GObject.signal_handler_find(
+        editable,
+        GObject.SignalMatchType.ID,
+        signalId,
+        GLib.quark_to_string(0),
+        null,
+        null,
+        null
+      );
+
+      GObject.signal_handler_block(editable, handlerId);
+
+      if (alphabet.includes(textUpperCase)) {
+        editable.insert_text(
+          textUpperCase,
+          textUpperCase.length,
+          editable.text.length
+        );
+      }
+
+      GObject.signal_handler_unblock(editable, handlerId);
+      GObject.signal_stop_emission(editable, signalId, GLib.quark_to_string(0));
+    };
 
     bindSettings = () => {
       this.settings = Gio.Settings.new(pkg.name);
