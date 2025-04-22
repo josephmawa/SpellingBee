00b664f0b2c6f9d4fdc1d59d3df7be8ccb8bc473
Joseph Mawa <186199059+josephmawa@users.noreply.github.com>
2025-04-22 19:07:52 +0300

    chore: create utils

---
+++ src/util.js
@@ -0,0 +1,18 @@
+export function getRandInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function shuffle(array) {
+  let currIdx = array.length,
+    tempVal,
+    randIdx;
+  while (0 !== currIdx) {
+    randIdx = Math.floor(Math.random() * currIdx);
+    currIdx -= 1;
+    tempVal = array[currIdx];
+    array[currIdx] = array[randIdx];
+    array[randIdx] = tempVal;
+  }
+
+  return array;
+}
