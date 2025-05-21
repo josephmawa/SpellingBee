710c07cd88c4abcda1db3786b364f4f3ad0efa94
Joseph Mawa <186199059+josephmawa@users.noreply.github.com>
2025-05-21 19:20:24 +0300

    chore: extract good words

---
+++ _data_/store/good-words/index.js
@@ -0,0 +1,7 @@
+module.exports.goodAcronyms = require("./good-acronyms.json");
+module.exports.goodBadWords = require("./good-bad-words.json");
+module.exports.goodCapitalCities = require("./good-capital-cities.json");
+module.exports.goodCountriesAndNationalities = require("./good-countries-and-nationalities.json");
+module.exports.goodNames = require("./good-names.json");
+module.exports.goodPlaces = require("./good-places.json");
+module.exports.goodSwearWords = require("./good-swear-words.json");
