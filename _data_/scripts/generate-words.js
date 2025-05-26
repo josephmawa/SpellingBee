8f7aed2f6e014de02d139bdbd505a7ce595cd4ad
Joseph Mawa <186199059+josephmawa@users.noreply.github.com>
2025-05-26 18:32:39 +0300

    chore: update puzzle data

--- _data_/scripts/generate-words.js
+++ _data_/scripts/generate-words.js
@@ -1,5 +1,5 @@
 /***
- * The code below generates new word list for an already created
+ * The code below generates new words for a created
  * puzzle list.
  *
  *  */
@@ -7,24 +7,24 @@
 const generatedPuzzles = require("../store/generated-puzzles.json");
 const dictionary = require("../store/tmp/dictionary.json");
 const removeWords = require("./remove-words.json");
+const { extractBadWords } = require("./util.js");
 
 const fs = require("fs/promises");
+const path = require("path");
 
-const removeWordsSet = new Set(removeWords)
+const removeWordsSet = new Set([...removeWords, ...extractBadWords()]);
 const cleanDict = dictionary.filter((word) => !removeWordsSet.has(word));
 
-
-// for (const puzzle of generatedPuzzles) {
-//   const letterSet = new Set(puzzle.letters);
-//   puzzle.words = cleanDict.filter((word) => {
-//     const wordSet = new Set(word);
-//     return letterSet.isSupersetOf(wordSet);
-//   });
-// }
-
-// const filePath =  "../store/generated-puzzles-temp.json";
-// fs.writeFile(filePath, JSON.stringify(generatedPuzzles, null, 2), {
-//   encoding: "utf-8",
-// })
-//   .then(() => console.log("Saved data to file"))
-//   .catch((error) => console.error(error));
+for (const puzzle of generatedPuzzles) {
+  const letterSet = new Set(puzzle.letters);
+  puzzle.words = cleanDict.filter((word) => {
+    const wordSet = new Set(word);
+    return letterSet.isSupersetOf(wordSet);
+  });
+}
+const filePath = path.join(__dirname, "..", "store", "generated-puzzles.json");
+fs.writeFile(filePath, JSON.stringify(generatedPuzzles, null, 2), {
+  encoding: "utf-8",
+})
+  .then(() => console.log("Saved data to file"))
+  .catch((error) => console.error(error));
